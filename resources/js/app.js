62516cc657f26747298f34754351a11b3fc07910
Shami03 <ihtisham11051@gmail.com>
2021-01-28 23:25:49 +0500

    Vue Router Configured

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+let routes = [
+  { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+  { path: '/profile', component: require('./components/Profile.vue').default }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+const app = new Vue({
+  el: '#app',
+  router
+}).$mount('#app')
